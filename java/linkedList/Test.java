be862626e7e26c5f821151312e6dafe5fda21204
Ruihan Zou <rhzou@amazon.com>
2021-01-28 00:03:23 +0800

    first commit

---
+++ java/linkedList/Test.java
@@ -0,0 +1,26 @@
+package com.day03;
+
+import com.day02.DynamicArray;
+import java.util.LinkedList;
+public class Test {
+    public static void main(String[] args) {
+        LinkedList list=new LinkedList();
+//        //list.add(1);
+//        list.add(null);
+//        list.add(1);
+//        list.get(0);
+//        System.out.println(list);
+//        //index>=size
+//        System.out.println(list.get(0));
+//         List linked=new LinkedList();
+//         linked.add(11);
+//         linked.add(33);
+//         linked.add(1,66);
+//        System.out.println(linked);
+//        linked.remove(0);
+//        System.out.println(linked);
+
+    }
+
+
+}
---
+++ python/linkedList/linkedList.py
@@ -0,0 +1,4 @@
+class ListNode:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.next = None
